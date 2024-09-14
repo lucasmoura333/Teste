4f3e1c6667385a9c666d6a586cae9cebe8ac6652
Lucas Moura <lborges.moura@gmail.com>
2024-09-13 21:17:44 -0400

    {001] Primeiro dia
    De 6 ideias que tive, 1 deu certo e executei toda meia boca.
    
    Amanhã vou apelar mais

--- scripts.js
+++ scripts.js
@@ -2,4 +2,21 @@
  $(document).ready(function() {
     // Exemplo de efeito de fadeIn nos cards
     $('.service-card').hide().fadeIn(1000);
+
+});
+
+
+$(document).ready(function() {
+    // Inicializar o carousel
+    $('.carouselExampleIndicators').carousel({
+        interval: 1500, // Define o intervalo para a transição automática dos slides (em milissegundos)
+        ride: 'carousel' // Inicializa o carousel automaticamente
+    });
+
+    // Configurar botões de navegação personalizados
+    $('.carousel-control-prev').html('<img src="assets/btn-esq.png" class="btnSlider">');
+    $('.carousel-control-next').html('<img src="assets/btn-dir.png" class="btnSlider">');
+
+    // Configurar a visibilidade dos indicadores de navegação
+    $('.carousel-indicators').addClass('d-none'); // Ocultar indicadores padrão se necessário
 });
