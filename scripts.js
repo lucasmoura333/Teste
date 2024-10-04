1ab0e392c79f84d6acc66032eb4de8ffe8f6d2b6
Lucas Moura <lborges.moura@gmail.com>
2024-10-04 15:40:26 -0400

    010 - Acabou Jéssica?
    >Tive que refazer algumas reestruturações até mesmo no JS pois os slickers estavam se influenciando.
    
    >> Retirei vários containers inuteis que estavam criando margens e espaçamentos desnecessários. Menos é mais!!!
    
    >>> Precisei reproduzir alguns botões e reposiciona-los porque ao redimensionar e reaproveitar classes, algumas divs quebraram...
    
    TODO: Refatorar o CSS para remover o peso morto mas necessito de mais prática para isso.

--- scripts.js
+++ scripts.js
@@ -1,175 +1,172 @@
 $(document).ready(function() {
-  // Certifique-se de inicializar o slick apenas uma vez
-  if (!$('.slick-slider').hasClass('slick-initialized')) {
-    $('.slick-slider-vitrini').slick({
+    // Slider Vitrine
+    if (!$('.slick-slider-vitrini').hasClass('slick-initialized')) {
+      $('.slick-slider-vitrini').slick({
         dots: true,
         arrows: true,
         prevArrow: '<button class="slick-prev-custom"><img src="assets/btn-esq.png" alt="Prev"></button>',
         nextArrow: '<button class="slick-next-custom"><img src="assets/btn-dir.png" alt="Next"></button>',
         infinite: true,
-        slidesToShow:1,
-        slidesToScroll:1,            
+        slidesToShow: 1,
+        slidesToScroll: 1,
         responsive: [
-            {
-                breakpoint: 1200,
-                settings: {
-                    rows: 1,
-                    slidesToScroll: 1,
-                    arrows: true,
-                    dots: true
-                }
-            },
-            {
-                breakpoint: 992,
-                settings: {
-                    slidesToShow: 1,
-                    dots: true
-                }
-            },
-            {
-                breakpoint: 768,
-                settings: {
-                    slidesToShow: 1,
-                    dots: true
-                }
-            },
-            {
-                breakpoint: 576,
-                settings: {
-                    slidesToShow: 1,
-                    arrows: false, 
-                    dots: true
-                }
+          {
+            breakpoint: 1200,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1,
+              dots: true,
             }
+          },
+          {
+            breakpoint: 992,
+            settings: {
+              slidesToShow: 1,
+              dots: true,
+            }
+          },
+          {
+            breakpoint: 768,
+            settings: {
+              slidesToShow: 1,
+              dots: true,
+            }
+          },
+          {
+            breakpoint: 576,
+            settings: {
+              slidesToShow: 1,
+              arrows: false,
+              dots: true,
+            }
+          }
         ]
-    });
-  }
-});
-
-$(document).ready(function(){    
-    $('.slick-slider-servicos').slick({
-        slidesToShow: 6,     
-        slidesToScroll: 2,          
-        arrows: true,       
+      });
+    }
+  
+    // Slider Serviços
+    if (!$('.slick-slider-servicos').hasClass('slick-initialized')) {
+      $('.slick-slider-servicos').slick({
+        slidesToShow: 6,
+        slidesToScroll: 1,
+        arrows: true,
         prevArrow: '<button class="slick-prev-servicos"><img src="assets/btn-esq.png" alt="Prev"></button>',
         nextArrow: '<button class="slick-next-servicos"><img src="assets/btn-dir.png" alt="Next"></button>',
-        infinite: true,       
+        infinite: true,
         responsive: [
-            {
-                breakpoint: 1200,
-                settings: {
-                    slidesToShow: 6,
-                }
-            },
-            {
-                breakpoint: 992,
-                settings: {
-                    slidesToShow: 4,
-                    
-                }
-            },
-            {
-                breakpoint: 768,
-                settings: {
-                    slidesToShow: 1,
-                    
-                }
-            },
-            {
-                breakpoint: 576,
-                settings: {
-                    slidesToShow: 1,
-                    arrows: false,                     
-                }
+          {
+            breakpoint: 1200,
+            settings: {
+              slidesToShow: 6,
+              slidesToScroll: 1,
             }
+          },
+          {
+            breakpoint: 992,
+            settings: {
+              slidesToShow: 4,
+            }
+          },
+          {
+            breakpoint: 768,
+            settings: {
+              slidesToShow: 1,
+            }
+          },
+          {
+            breakpoint: 576,
+            settings: {
+              slidesToShow: 1,
+              arrows: false,
+            }
+          }
         ]
-    });
-});
-
-
-$(document).ready(function(){    
-    $('.slickerNoticia').slick({
-        slidesToShow: 4,     
-        slidesToScroll: 2,          
-        arrows: true, 
-        autoplay: true,     
-         
+      });
+    }
+  
+    // Slider de Notícias
+    if (!$('.slickerNoticia').hasClass('slick-initialized')) {
+      $('.slickerNoticia').slick({
+        slidesToShow: 4,
+        slidesToScroll: 2,
+        arrows: true,
+        autoplay: true,
         prevArrow: '<button class="slick-prev-noticias"><img src="assets/btn-esq.png" alt="Prev"></button>',
         nextArrow: '<button class="slick-next-noticias"><img src="assets/btn-dir.png" alt="Next"></button>',
-        infinite: true,       
+        infinite: true,
         responsive: [
-            {
-                breakpoint: 1200,
-                settings: {
-                    slidesToShow: 4,
-                }
-            },
-            {
-                breakpoint: 992,
-                settings: {
-                    slidesToShow: 4,
-                    
-                }
-            },
-            {
-                breakpoint: 768,
-                settings: {
-                    slidesToShow: 3,
-                    
-                }
-            },
-            {
-                breakpoint: 576,
-                settings: {
-                    slidesToShow: 2,
-                    arrows: false,                     
-                }
+          {
+            breakpoint: 1200,
+            settings: {
+              slidesToShow: 4,
             }
+          },
+          {
+            breakpoint: 992,
+            settings: {
+              slidesToShow: 4,
+            }
+          },
+          {
+            breakpoint: 768,
+            settings: {
+              slidesToShow: 3,
+            }
+          },
+          {
+            breakpoint: 576,
+            settings: {
+              slidesToShow: 2,
+              arrows: false,
+            }
+          }
         ]
-    });
-});
-
-
-$(document).ready(function(){    
-    $('.slickerGaleria').slick({
-        slidesToShow: 3,     
-        slidesToScroll: 1,          
-        arrows: true,   
-        centerMode: true,     
+      });
+    }
+  
+    // Slider Galeria
+    if (!$('.slickerGaleria').hasClass('slick-initialized')) {
+      $('.slickerGaleria').slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        arrows: true,
+        centerMode: true,
+        centerPadding: '10px',
         prevArrow: '<button class="slick-prev-galeria"><img src="assets/btn-esq.png" alt="Prev"></button>',
         nextArrow: '<button class="slick-next-galeria"><img src="assets/btn-dir.png" alt="Next"></button>',
-        infinite: true,       
+        infinite: true,
         responsive: [
-            {
-                breakpoint: 1200,
-                settings: {
-                    slidesToShow: 3,
-                }
-            },
-            {
-                breakpoint: 992,
-                settings: {
-                    slidesToShow: 3,
-                    
-                }
-            },
-            {
-                breakpoint: 768,
-                settings: {
-                    slidesToShow: 2,
-                    
-                }
-            },
-            {
-                breakpoint: 576,
-                settings: {
-                    slidesToShow: 2,
-                    arrows: false,                     
-                }
+          {
+            breakpoint: 1200,
+            settings: {
+              slidesToShow: 3,
             }
+          },
+          {
+            breakpoint: 992,
+            settings: {
+              slidesToShow: 3,
+            }
+          },
+          {
+            breakpoint: 768,
+            settings: {
+              slidesToShow: 2,
+              centerMode: true,
+              centerPadding: '0',
+            }
+          },
+          {
+            breakpoint: 576,
+            settings: {
+              slidesToShow: 2,
+              arrows: false,
+              centerMode: true,
+              centerPadding: '0',
+            }
+          }
         ]
-    });
-});
-
-
-
+      });
+    }
+  });
+  
